5e780a758fcc68c9185d8bfe849c8de2eac797e3
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2019-02-08 21:48:12 +0100

    Start to implement functionality

---
+++ paintera_tools/__init__.py
@@ -0,0 +1,1 @@
+from .merge_projects import merge_projects
---
+++ paintera_tools/merge_projects.py
@@ -0,0 +1,14 @@
+import os
+import z5py
+
+
+def merge_projects(project_a, project_b, new_project, strategy=''):
+    """ Merge to paintera projects.
+
+    Args:
+        project_a (str): path to first project folder
+        project_b (str): path to second project folder
+        new_project (str): path to output project folder
+        strategy (str): merge strategy TODO
+    """
+    pass
