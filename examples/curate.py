24f397d4256b0381e6cc8dc9677cea453be2b2db
Constantin Pape <c.pape@gmx.net>
2019-06-17 16:29:24 +0200

    Update curation example

--- examples/curate.py
+++ examples/curate.py
@@ -9,11 +9,11 @@ def interactive_splitting():
 
     path = '/g/kreshuk/data/cremi/example/sampleA.n5'
     key = 'paintera'
-    aff_key = 'predictions/full_affs'
+    aff_key = 'volumes/affinities'
 
     tmp_folder = 'tmp_split'
 
-    n_jobs = 8
+    n_jobs = 4
     interactive_splitter(path, key, path, aff_key,
                          tmp_folder, 'local', n_jobs, n_jobs)
 
@@ -24,23 +24,23 @@ def batch_splitting():
 
     path = '/g/kreshuk/data/cremi/example/sampleA.n5'
     key = 'paintera'
-    aff_key = 'predictions/full_affs'
+    aff_key = 'volumes/affinities'
 
     tmp_folder = 'tmp_split'
 
-    segment_id = 200194
+    segment_id = 198625
     with open('./split_state_object_%i.json' % segment_id) as f:
         seed_dict = json.load(f)
 
     segment_ids = [segment_id]
     all_seed_fragments = [[seed_dict[seed_id] for seed_id in seed_dict]]
 
-    n_jobs = 8
+    n_jobs = 4
     batch_splitter(path, key, path, aff_key,
                    segment_ids, all_seed_fragments,
                    tmp_folder, 'local', n_jobs, n_jobs)
 
 
 if __name__ == '__main__':
-    interactive_splitting()
-    # batch_splitting()
+    # interactive_splitting()
+    batch_splitting()
