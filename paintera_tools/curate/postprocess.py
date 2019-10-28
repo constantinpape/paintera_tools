f330c6e7326b89a7bc41aae122d3dc114ca51832
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2019-10-28 11:45:43 +0100

    Update postprocessing workflow

--- paintera_tools/curate/postprocess.py
+++ paintera_tools/curate/postprocess.py
@@ -6,8 +6,24 @@ import numpy as np
 
 from cluster_tools.postprocess import SizeFilterAndGraphWatershedWorkflow
 from cluster_tools.postprocess import ConnectedComponentsWorkflow
+from cluster_tools.write import WriteLocal, WriteSlurm
 from ..util import compute_graph_and_weights, assignment_saver, update_max_id, PAINTERA_IGNORE_LABEL
 from ..serialize import serialize_from_commit
+
+
+def write_postprocessed(in_path, in_key,
+                        assignment_path, assignment_key,
+                        out_path, out_key,
+                        tmp_folder, config_dir,
+                        target, max_jobs):
+    task = WriteLocal if target == 'local' else WriteSlurm
+    t = task(tmp_folder=tmp_folder, config_dir=config_dir, max_jobs=max_jobs,
+             input_path=in_path, input_key=in_key,
+             output_path=out_path, output_key=out_key,
+             assignment_path=assignment_path, assignment_key=assignment_key,
+             identifier='write-postprocessed')
+    ret = luigi.build([t], local_scheduler=True)
+    assert ret, "Write failed"
 
 
 def make_graph_assignments(f, node_ids, assignments, out_key, n_threads):
@@ -29,13 +45,53 @@ def make_graph_assignments(f, node_ids, assignments, out_key, n_threads):
     ds[:] = node_labels
 
 
+def update_assignments_inplace(paintera_path, paintera_key,
+                               input_path, input_key,
+                               n_threads, backup_assignments):
+    assignment_key = 'fragment-segment-assignment'
+
+    # backup the assignments if specified
+    ff = z5py.File(paintera_path)
+    ds_ass = ff[paintera_key][assignment_key]
+    chunks = ds_ass.chunks
+    if backup_assignments:
+        bkp_key = os.path.join(paintera_key, 'assignments-bkp')
+        ds_ass.n_threads = n_threads
+        assignments = ds_ass[:].T
+        assignment_saver(paintera_path, bkp_key, n_threads,
+                         assignments, chunks)
+    # 7.) load the new assignments, bring to paintera format and save
+    f = z5py.File(input_path)
+    ds_ass = f[input_key]
+    ds_ass.n_threads = n_threads
+    new_assignments = ds_ass[:]
+
+    new_assignments[1:] += len(new_assignments)
+    new_assignments = np.concatenate([np.arange(len(new_assignments), dtype='uint64')[:, None],
+                                      new_assignments[:, None]], axis=1)
+    assignment_saver(paintera_path, os.path.join(paintera_key, assignment_key),
+                     n_threads, new_assignments, chunks)
+    update_max_id(paintera_path, paintera_key, new_assignments)
+
+
 def postprocess(paintera_path, paintera_key,
                 boundary_path, boundary_key,
                 tmp_folder, target, max_jobs, n_threads,
-                size_threshold=0, label=False,
-                backup_assignments=True):
+                size_threshold=None, target_number=None,
+                label=False, backup_assignments=True,
+                output_path=None, output_key=None):
 
-    if not label and size_threshold == 0:
+    if output_path is None:
+        assert output_key is None
+        inplace = True
+    else:
+        assert output_key is not None
+        inplace = False
+
+    assert not ((size_threshold is not None) and (target_number is not None)), "Invalid option for size filtering"
+    run_size_filter = size_threshold is not None or target_number is not None
+
+    if not label and not run_size_filter:
         print("Neither size filtering nor label selected; doing nothing")
         return
 
@@ -97,7 +153,8 @@ def postprocess(paintera_path, paintera_key,
 
         configs = task.get_config()
         conf = configs['graph_connected_components']
-        conf.update({'threads_per_job': n_threads, 'mem_limit': 256, 'time_limit': 240, 'paintera_hack': paintera_hack})
+        conf.update({'threads_per_job': n_threads, 'mem_limit': 256,
+                     'time_limit': 240, 'paintera_hack': paintera_hack})
         with open(os.path.join(config_dir, 'graph_connected_components.config'), 'w') as f:
             json.dump(conf, f)
 
@@ -119,7 +176,8 @@ def postprocess(paintera_path, paintera_key,
         current_seg_key = cc_seg_key
 
     # 5.) run size filter work-flow if size threshold
-    if size_threshold > 0:
+    if run_size_filter:
+
         # TODO implement this as well
         if paintera_hack:
             raise NotImplementedError("Size filtering not implemented for paintera hack")
@@ -130,6 +188,8 @@ def postprocess(paintera_path, paintera_key,
         with open(os.path.join(config_dir, 'graph_watershed_assignments.config'), 'w') as f:
             json.dump(conf, f)
 
+        # we only bother to relabel if we don't run inplace
+        relabel_thresholded = not inplace
         filtered_key = 'assignments/size_filtered'
         t = task(tmp_folder=tmp_folder, config_dir=config_dir,
                  max_jobs=max_jobs, target=target,
@@ -137,33 +197,20 @@ def postprocess(paintera_path, paintera_key,
                  graph_key='s0/graph', features_key='features',
                  path=exp_path, segmentation_key=current_seg_key,
                  assignment_key=current_ass_key,
-                 size_threshold=size_threshold,
-                 relabel=False, output_path=exp_path,
+                 size_threshold=size_threshold, target_number=target_number,
+                 relabel=relabel_thresholded, output_path=exp_path,
                  assignment_out_key=filtered_key)
         ret = luigi.build([t], local_scheduler=True)
         assert ret, "Size filter failed"
         current_ass_key = filtered_key
 
-    # 6.) backup the assignments if specified
-    ff = z5py.File(paintera_path)
-    ds_ass = ff[paintera_key][assignment_key]
-    chunks = ds_ass.chunks
-    if backup_assignments:
-        bkp_key = os.path.join(paintera_key, 'assignments-bkp')
-        ds_ass.n_threads = n_threads
-        assignments = ds_ass[:].T
-        assignment_saver(paintera_path, bkp_key, n_threads,
-                         assignments, chunks)
-
-    # 7.) load the new assignments, bring to paintera format and save
-    f = z5py.File(exp_path)
-    ds_ass = f[current_ass_key]
-    ds_ass.n_threads = n_threads
-    new_assignments = ds_ass[:]
-
-    new_assignments[1:] += len(new_assignments)
-    new_assignments = np.concatenate([np.arange(len(new_assignments), dtype='uint64')[:, None],
-                                      new_assignments[:, None]], axis=1)
-    assignment_saver(paintera_path, os.path.join(paintera_key, assignment_key),
-                     n_threads, new_assignments, chunks)
-    update_max_id(paintera_path, paintera_key, new_assignments)
+    if inplace:
+        update_assignments_inplace(paintera_path, paintera_key,
+                                   exp_path, current_ass_key,
+                                   n_threads, backup_assignments)
+    else:
+        write_postprocessed(paintera_path, os.path.join(paintera_key, data_key),
+                            exp_path, current_ass_key,
+                            output_path, output_key,
+                            tmp_folder, config_dir,
+                            target, max_jobs)
