08f051e97481dfcaaba2afa9b7ee0640fd253560
Constantin Pape <constantin.pape@embl.de>
2020-07-20 23:22:52 +0200

    Update post-processing workflow

--- paintera_tools/curate/postprocess.py
+++ paintera_tools/curate/postprocess.py
@@ -4,10 +4,12 @@ import luigi
 import z5py
 import numpy as np
 
+from cluster_tools.node_labels import NodeLabelWorkflow
 from cluster_tools.postprocess import SizeFilterAndGraphWatershedWorkflow
 from cluster_tools.postprocess import ConnectedComponentsWorkflow
+from cluster_tools.relabel import RelabelWorkflow
 from cluster_tools.write import WriteLocal, WriteSlurm
-from ..util import compute_graph_and_weights, assignment_saver, update_max_id, PAINTERA_IGNORE_LABEL
+from ..util import compute_graph_and_weights, assignment_saver, update_max_id
 from ..serialize import serialize_from_commit
 
 
@@ -55,6 +57,36 @@ def update_assignments_inplace(paintera_path, paintera_key,
     update_max_id(paintera_path, paintera_key, new_assignments)
 
 
+def copy_watersheds(in_path, in_key,
+                    ws_path, ws_key, exp_path,
+                    tmp_folder, config_dir,
+                    target, max_jobs):
+    task = RelabelWorkflow
+    t = task(tmp_folder=tmp_folder, config_dir=config_dir,
+             target=target, max_jobs=max_jobs,
+             input_path=in_path, input_key=in_key,
+             assignment_path=exp_path, assignment_key='node_labels/relabel_ws',
+             output_path=ws_path, output_key=ws_key)
+    ret = luigi.build([t], local_scheduler=True)
+    assert ret
+
+
+def get_new_assignments(ws_path, ws_key,
+                        seg_path, seg_key,
+                        out_path, out_key,
+                        tmp_folder, config_dir,
+                        target, max_jobs):
+    task = NodeLabelWorkflow
+    t = task(tmp_folder=tmp_folder, config_dir=config_dir,
+             target=target, max_jobs=max_jobs,
+             ws_path=ws_path, ws_key=ws_key,
+             input_path=seg_path, input_key=seg_key,
+             output_path=out_path, output_key=out_key,
+             prefix='new-assignments')
+    ret = luigi.build([t], local_scheduler=True)
+    assert ret
+
+
 def postprocess(paintera_path, paintera_key,
                 boundary_path, boundary_key,
                 tmp_folder, target, max_jobs, n_threads,
@@ -70,7 +102,8 @@ def postprocess(paintera_path, paintera_key,
         assert output_key is not None
         inplace = False
 
-    assert not ((size_threshold is not None) and (target_number is not None)), "Invalid option for size filtering"
+    assert not ((size_threshold is not None) and (target_number is not None)),\
+        "Invalid option for size filtering"
     run_size_filter = size_threshold is not None or target_number is not None
 
     if not label and not run_size_filter:
@@ -99,49 +132,34 @@ def postprocess(paintera_path, paintera_key,
                           tmp_serialize, max_jobs, target,
                           relabel_output=relabel)
 
-    # 2.) compute graph and weigthts
+    # 2.a) copy and relabel the watersheds to avoid horror of paintera ignore ids
+    ws_path = exp_path
+    ws_key = 'watersheds'
+    copy_watersheds(paintera_path, os.path.join(paintera_key, data_key),
+                    ws_path, ws_key, exp_path,
+                    tmp_folder, config_dir,
+                    target, max_jobs)
+
+    # 2.b) get the assignments after relabeling
+    current_ass_key = 'assignments/relabeled_assignments'
+    get_new_assignments(ws_path, ws_key,
+                        exp_path, current_seg_key,
+                        exp_path, current_ass_key,
+                        tmp_folder, config_dir,
+                        target, max_jobs)
+
+    # 3.) compute graph and weigthts
     compute_graph_and_weights(boundary_path, boundary_key,
-                              paintera_path, os.path.join(paintera_key, data_key),
+                              ws_path, ws_key,
                               exp_path, tmp_folder, target, max_jobs)
 
-    # 3.) save the relabeled assignments in a format that can be ingested by
-    # the graph watershed workflow
-    relabeled_assignment_path = os.path.join(tmp_serialize, 'assignments.n5')
-    relabeled_assignment_key = 'assignments'
-    with z5py.File(relabeled_assignment_path, 'r') as f:
-        ds_relabeled = f[relabeled_assignment_key]
-        ds_relabeled.n_threads = n_threads
-        relabeled_assignments = ds_relabeled[:]
-
-    # need to make assignments completely dense
-    node_ids, relabeled_assignments = relabeled_assignments[:, 0], relabeled_assignments[:, 1]
-
-    # hack for the paintera ignore id
-    paintera_hack = False
-    if PAINTERA_IGNORE_LABEL in node_ids:
-        paintera_hack = True
-        node_ids, relabeled_assignments = node_ids[:-1], relabeled_assignments[:-1]
-
-    n_nodes = int(node_ids.max()) + 1
-    new_assignments = np.zeros(n_nodes, dtype='uint64')
-    new_assignments[node_ids] = relabeled_assignments
-
-    f = z5py.File(exp_path)
-    current_ass_key = 'assignments/relabeled_assignments'
-    chunks1d = (min(len(relabeled_assignments), 1000000),)
-    ds_out = f.require_dataset(current_ass_key, shape=new_assignments.shape,
-                               chunks=chunks1d, compression='gzip', dtype='uint64')
-    ds_out[:] = new_assignments
-
-    # FIXME something goes wrong ig we use both connected components and size filtering
     # 4.) run connected components if selected
     if label:
         task = ConnectedComponentsWorkflow
 
         configs = task.get_config()
         conf = configs['graph_connected_components']
-        conf.update({'threads_per_job': n_threads, 'mem_limit': 256,
-                     'time_limit': 240, 'paintera_hack': paintera_hack})
+        conf.update({'threads_per_job': n_threads, 'mem_limit': 64, 'time_limit': 240})
         with open(os.path.join(config_dir, 'graph_connected_components.config'), 'w') as f:
             json.dump(conf, f)
 
@@ -150,8 +168,7 @@ def postprocess(paintera_path, paintera_key,
         t = task(tmp_folder=tmp_folder, config_dir=config_dir,
                  max_jobs=max_jobs, target=target,
                  problem_path=exp_path, graph_key='s0/graph',
-                 path=paintera_path,
-                 fragments_key=os.path.join(paintera_key, data_key),
+                 path=ws_path, fragments_key=ws_key,
                  assignment_path=exp_path,
                  assignment_key=current_ass_key,
                  output_path=exp_path,
@@ -165,13 +182,10 @@ def postprocess(paintera_path, paintera_key,
     # 5.) run size filter work-flow if size threshold
     if run_size_filter:
 
-        # TODO implement this as well
-        if paintera_hack:
-            raise NotImplementedError("Size filtering not implemented for paintera hack")
         task = SizeFilterAndGraphWatershedWorkflow
         configs = task.get_config()
         conf = configs['graph_watershed_assignments']
-        conf.update({'threads_per_job': n_threads, 'mem_limit': 256, 'time_limit': 240})
+        conf.update({'threads_per_job': n_threads, 'mem_limit': 64, 'time_limit': 240})
         with open(os.path.join(config_dir, 'graph_watershed_assignments.config'), 'w') as f:
             json.dump(conf, f)
 
@@ -194,7 +208,7 @@ def postprocess(paintera_path, paintera_key,
                                    exp_path, current_ass_key,
                                    n_threads, backup_assignments)
     else:
-        write_postprocessed(paintera_path, os.path.join(paintera_key, data_key),
+        write_postprocessed(ws_path, ws_key,
                             exp_path, current_ass_key,
                             output_path, output_key,
                             tmp_folder, config_dir,
